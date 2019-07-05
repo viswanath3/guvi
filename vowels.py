2e2024e2490f9b017c5130fec8981f2beccc6187
Viswanath <35722620+viswanath3@users.noreply.github.com>
2019-07-05 10:22:04 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -2,7 +2,7 @@ list_a=['a','e','i','o','u']
 list_b=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 char=str(input())
 if(char in list_a):
-    print("Vowels")
+    print("Vowel")
 elif(char in list_b):
     print("Consonant")
 else:
