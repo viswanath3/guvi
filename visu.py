fee3bdfc5f0f935e7494885b621abbeedac49f49
Viswanath <35722620+viswanath3@users.noreply.github.com>
2019-07-05 10:27:34 +0530

    Create visu.py

---
+++ visu.py
@@ -0,0 +1,6 @@
+alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+char=str(input())
+if(char in alphabets):
+    print("Alphabet")
+else:
+    print("No")
